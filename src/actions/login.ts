02095885065e33ac4819355b7dcb3f5072488efe
Ruhtra <kawanarthurskate@gmail.com>
2025-01-20 14:33:38 -0300

    save

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,37 +1,35 @@
-'use server'
+"use server";
 
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "../../routes";
 import { AuthError } from "next-auth";
 
 export async function login(values: any) {
-    console.log(values);
+  console.log(values);
 
-    const { email, password } = values
-    
-    try {
-        await signIn('credentials', {
-            email,
-            password,
-            redirectTo: DEFAULT_LOGIN_REDIRECT
-        })
-    } catch (error) {
-        if (error instanceof AuthError) {
+  const { email, password } = values;
 
-            console.log('balbsbfklsbfusisnfkjsnfsj');
-            
-            console.log(error);
-            
-            switch (error.type) {
-                case "CredentialsSignin":
-                    return { error: "Invalid credentials" }
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+      // redirectTo: DEFAULT_LOGIN_REDIRECT
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      console.log("balbsbfklsbfusisnfkjsnfsj");
 
-                default: return { error: "An error occurred" }
+      console.log(error);
 
-            }
-        }
-        throw error
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+
+        default:
+          return { error: "An error occurred" };
+      }
     }
-
-
+    throw error;
+  }
 }
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,68 +1,61 @@
-'use client'
+"use client";
 
-import { login } from '@/actions/login';
-import React, { useTransition, useState } from 'react';
+import { login } from "@/actions/login";
+import React, { useTransition, useState } from "react";
 
 export default function LoginPage() {
-    const [isPending, startTransition] = useTransition();
-    const [message, setMessage] = useState<string | null>(null);
-    const [error, setError] = useState<string | null>(null);
+  const [isPending, startTransition] = useTransition();
+  const [message, setMessage] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
-    async function submitForm(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const values = Object.fromEntries(formData.entries());
+  async function submitForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const values = Object.fromEntries(formData.entries());
 
-        startTransition(async () => {
-            try {
-                const response = await login(values);
-                console.log(response?.error);
-                
-                if (!!response?.error) {
-                    setError(response.error)
-                    setMessage(null);
-                } else {
-                    setError(null);
-                    setMessage('Login successful!');
-                }
+    startTransition(async () => {
+      try {
+        console.log("aqui");
+        const response = await login(values);
+        console.log(response?.error);
 
-                // setError(null);
-                // setMessage('Login successful!');
-            } catch (err) {
-                setError('Login failed. Please try again.');
-                setMessage(null);
-            }
-        });
-    }
+        if (!!response?.error) {
+          setError(response.error);
+          setMessage(null);
+        } else {
+          setError(null);
+          setMessage("Login successful!");
+        }
 
-    return (
+        // setError(null);
+        // setMessage('Login successful!');
+      } catch (err) {
+        console.log(err);
+
+        setError("Login failed. Please try again.");
+        setMessage(null);
+      }
+    });
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={submitForm}>
         <div>
-            <h2>Login</h2>
-            <form onSubmit={submitForm}>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        required
-                    />
-                </div>
-                <button type="submit" disabled={isPending}>
-                    {isPending ? 'Logging in...' : 'Login'}
-                </button>
-            </form>
-            {message && <p style={{ color: 'green' }}>{message}</p>}
-            {error && <p style={{ color: 'red' }}>{error}</p>}
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
         </div>
-    );
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit" disabled={isPending}>
+          {isPending ? "Logging in..." : "Login"}
+        </button>
+      </form>
+      {message && <p style={{ color: "green" }}>{message}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
 }
